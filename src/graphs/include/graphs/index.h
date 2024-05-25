dcee5e057dd9a55d49b1d371218557e5e568f43e
Pierre Dejoue <pierre.dejoue@gmail.com>
2024-05-25 20:45:12 +0200

    Update graphs::IndexTraits

--- src/graphs/include/graphs/index.h
+++ src/graphs/include/graphs/index.h
@@ -14,9 +14,15 @@ namespace graphs {
 template <typename I>
 struct IndexTraits
 {
-    static constexpr bool is_valid() { return std::is_integral_v<I> && std::is_unsigned_v<I>; }
-    static constexpr I undef() { return std::numeric_limits<I>::max(); }
-    static constexpr I max_valid_index() { return std::numeric_limits<I>::max() - I{1}; }
+    static constexpr bool is_valid()        noexcept { return std::is_integral_v<I> && std::is_unsigned_v<I>; }
+    static constexpr I    undef()           noexcept { return std::numeric_limits<I>::max(); }
+    static constexpr I    max_valid_index() noexcept { return std::numeric_limits<I>::max() - I{1}; }
 };
+
+template <typename I, std::enable_if_t<IndexTraits<I>::is_valid(), bool> = true>
+constexpr bool is_defined(I index) noexcept
+{
+    return index != IndexTraits<I>::undef();
+}
 
 } // namespace graphs
